0503f87c75e833fd10d588c542b8ab8d0d7365ba
Marco Dugatto <dughi.boogie@gmail.com>
2025-03-31 22:13:28 +0200

    Final version of Diary, now every feature of it should work

--- spaghetti-invasion/Scripts/Diary/Diary.gd
+++ spaghetti-invasion/Scripts/Diary/Diary.gd
@@ -8,14 +8,14 @@ enum LastPageWritten {
 	RIGHT
 }
 var last_page_written: LastPageWritten = LastPageWritten.BLANK
-@onready var left_pages_content = $Body/LeftCoverPivot/LeftPages/Content/SubViewport
-@onready var right_pages_content = $Body/RightCoverPivot/RightPages/Content/SubViewport
+@onready var left_page_content = $Body/LeftCoverPivot/LeftPages/Content/SubViewport
+@onready var right_page_content = $Body/RightCoverPivot/RightPages/Content/SubViewport
 var discovered_diary_pages: Array[DiaryPage]
 @onready var empty_diary_page_scene = preload("res://Scenes/Diary/DiaryPages/EmptyDiaryPage.tscn")
+var current_diary_page = 0
 
 var all_diary_entries_path = "res://Scenes/Diary/DiaryPages/"
 var all_diary_entries: Array
-
 
 var is_diary_out = false
 @onready var animation_player = $AnimationPlayer
@@ -59,8 +59,13 @@ func _unhandled_key_input(event: InputEvent) -> void:
 			put_away()
 		else:
 			pull_out()
-		
 		is_diary_out = !is_diary_out
+	
+	if is_diary_out:
+		if event.is_action_pressed("left"):
+			turn_page(false)
+		if event.is_action_pressed("right"):
+			turn_page(true)
 
 
 func put_away():
@@ -76,6 +81,7 @@ func pull_out():
 func add_diary_page(area_name: ClueData.DiaryPages):
 	# Add page to discovered diary pages
 	var new_diary_page = empty_diary_page_scene.instantiate()
+	new_diary_page.diary_page_number = discovered_diary_pages.size()
 	discovered_diary_pages.append(new_diary_page)
 	
 	# Change DiaryPage Label text
@@ -83,25 +89,31 @@ func add_diary_page(area_name: ClueData.DiaryPages):
 	
 	 # Check add left or right page
 	if(last_page_written == LastPageWritten.BLANK or last_page_written == LastPageWritten.RIGHT):
-		left_pages_content.add_child(new_diary_page)
+		left_page_content.add_child(new_diary_page)
+		if(last_page_written == LastPageWritten.RIGHT):
+			go_to_page(discovered_diary_pages.size() - 1)
 		last_page_written = LastPageWritten.LEFT
-	else:
-		right_pages_content.add_child(new_diary_page)
+	
+	elif(last_page_written == LastPageWritten.LEFT):
+		right_page_content.add_child(new_diary_page)
 		last_page_written = LastPageWritten.RIGHT
 
 
 func add_clue_entry(clue_data: ClueData):
 	# Search for diary page
-	var new_entry_diary_page: DiaryPage
+	var diary_page: DiaryPage
 	var new_diary_entry: DiaryEntry
 	
 	for page in discovered_diary_pages:
 		if page.diary_page_ID == clue_data.diary_page_ID:
-			new_entry_diary_page = page
+			diary_page = page
 			break
-	if new_entry_diary_page == null:
-		printerr("No diary page found for interacted clue!")
+	if diary_page == null:
+		printerr("No diary page found for new clue!")
 		return
+	
+	# Go to found diary page
+	go_to_page(diary_page.diary_page_number)
 	
 	# Search for diary entry scene
 	for entry in all_diary_entries:
@@ -109,7 +121,58 @@ func add_clue_entry(clue_data: ClueData):
 			new_diary_entry = entry
 			break
 	if new_diary_entry == null:
-		printerr("No diary entry found for interacted clue!")
-		return	
+		printerr("No diary entry found for new clue!")
+		return
+	
 	# Add entry to diary page
-	new_entry_diary_page.add_child(new_diary_entry)
+	diary_page.add_new_entry(new_diary_entry)
+	diary_page.load_page()
+
+
+func clear_current_pages() -> void:
+	var page_content_to_remove
+	
+	if left_page_content.get_child_count() > 0:
+		page_content_to_remove = left_page_content.get_child(0)
+		left_page_content.remove_child(page_content_to_remove)
+	if right_page_content.get_child_count() > 0:
+		page_content_to_remove = right_page_content.get_child(0)
+		right_page_content.remove_child(page_content_to_remove)
+
+
+func go_to_page(page: int) -> void:
+	# Always point to left page to keep track of which one should be displayed
+	if page % 2 != 0:
+		current_diary_page = page - 1
+	else:
+		current_diary_page = page
+	
+	clear_current_pages()
+	
+	if(discovered_diary_pages[page].get_parent() == null):
+		left_page_content.add_child(discovered_diary_pages[current_diary_page])
+	else:
+		discovered_diary_pages[page].reparent(left_page_content)
+	
+	# This means there's another page to load on the right
+	if discovered_diary_pages.size() >= current_diary_page + 2:
+		if(discovered_diary_pages[page + 1].get_parent() == null):
+			right_page_content.add_child(discovered_diary_pages[current_diary_page + 1])
+		else:
+			discovered_diary_pages[page + 1].reparent(right_page_content)
+
+
+func turn_page(right: bool) -> void:
+	# Left limit
+	if (current_diary_page == 0 and !right):
+		printerr("No pages left to turn!")
+		return
+	# Right limit
+	if (current_diary_page >= discovered_diary_pages.size() - 2 and right):
+		printerr("No pages left to turn!")
+		return
+	
+	if right:
+		go_to_page(current_diary_page + 2)
+	else:
+		go_to_page(current_diary_page - 2)
--- spaghetti-invasion/Scripts/Diary/DiaryPage.gd
+++ spaghetti-invasion/Scripts/Diary/DiaryPage.gd
@@ -3,10 +3,35 @@ extends Control
 
 
 @onready var page_title = $PageTitle
+@onready var page_entries = $PageEntries
 
 var diary_page_ID: ClueData.DiaryPages
+var discovered_page_entries_scenes: Array[DiaryEntry]
+var diary_page_number: int
 
 
 func _ready() -> void:
 	var diary_page_name: String = ClueData.DiaryPages.keys()[diary_page_ID]
 	page_title.text = diary_page_name.replace("_", " ")
+	load_page()
+
+
+func add_new_entry(new_entry_scene: DiaryEntry):
+	discovered_page_entries_scenes.append(new_entry_scene)
+
+
+func clear_page() -> void:
+	if page_entries == null or page_entries.get_child_count() == 0:
+		return
+	
+	for entry in page_entries.get_children():
+		page_entries.remove_child(entry)
+
+
+func load_page() -> DiaryPage:
+	clear_page()
+	
+	for entry in discovered_page_entries_scenes:
+		page_entries.add_child(entry)
+	
+	return self
